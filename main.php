23e2bd42abf22d2a3eec94b6546d975e914a5461
ChrisScott9456 <christopherscott9456@gmail.com>
2017-07-08 17:31:23 -0400

    Initial commit

---
+++ main.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <title>PHP Project</title>
+  <link rel = "stylesheet" type="text/css" href="main.css">
+
+</head>
+
+<body>
+
+  <?php
+
+    $_SESSION["txt"] = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if(array_key_exists('submit',$_POST)){
+        roll();
+      }
+    }
+
+    function roll() {
+      $_SESSION["txt"] = "rolling";
+    }
+  ?>
+
+  <form method="post">
+    <input type="submit" name="submit" id="submit" value="Submit">
+  </form>
+
+  <div class="container">
+    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
+    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
+    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
+  </div>
+
+</body>
+
+</html>
