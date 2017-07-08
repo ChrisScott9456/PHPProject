fc36cd4ff77ddd384293b9f362189977ba499a57
ChrisScott9456 <christopherscott9456@gmail.com>
2017-07-08 18:15:53 -0400

    dice 2
    -Added stop button
    -Added variables for each of the three die

--- main.php
+++ main.php
@@ -14,31 +14,69 @@
 
   <?php
 
-    $_SESSION["txt"] = "";
+    $_SESSION["side1"] = "";
+    $_SESSION["side2"] = "";
+    $_SESSION["side3"] = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       if(array_key_exists('submit',$_POST)){
         roll();
+      }else{
+        stopRoll();
       }
     }
 
     function roll() {
-      $_SESSION["txt"] = "rolling";
+      $_SESSION["side1"] = "rolling";
+      $_SESSION["side2"] = "rolling";
+      $_SESSION["side3"] = "rolling";
     }
 
     function stopRoll() {
-      $_SESSION["txt"] = mt_rand(1,6);
+      switch(mt_rand(1,6)) {
+        case 1:
+          $_SESSION["side1"] = "one";
+          $_SESSION["side2"] = "one";
+          $_SESSION["side3"] = "one";
+          break;
+        case 2:
+          $_SESSION["side1"] = "two";
+          $_SESSION["side2"] = "two";
+          $_SESSION["side3"] = "two";
+          break;
+        case 3:
+          $_SESSION["side1"] = "three";
+          $_SESSION["side2"] = "three";
+          $_SESSION["side3"] = "three";
+          break;
+        case 4:
+          $_SESSION["side1"] = "four";
+          $_SESSION["side2"] = "four";
+          $_SESSION["side3"] = "four";
+          break;
+        case 5:
+          $_SESSION["side1"] = "five";
+          $_SESSION["side2"] = "five";
+          $_SESSION["side3"] = "five";
+          break;
+        case 6:
+          $_SESSION["side1"] = "six";
+          $_SESSION["side2"] = "six";
+          $_SESSION["side3"] = "six";
+          break;
+      }
     }
   ?>
 
   <form method="post">
-    <input type="submit" name="submit" id="submit" value="Submit">
+    <input type="submit" name="submit" value="ROLL">
+    <input type="submit" name="submit2" value="STOP">
   </form>
 
   <div class="container">
-    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
-    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
-    <div class="die <?php echo $_SESSION["txt"] ?>"></div>
+    <div class="die <?php echo $_SESSION["side1"] ?>"></div>
+    <div class="die <?php echo $_SESSION["side2"] ?>"></div>
+    <div class="die <?php echo $_SESSION["side3"] ?>"></div>
   </div>
 
 </body>
